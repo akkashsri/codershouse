3d81b6d55d713bf7c796cf47208d68f49a0c918a
Akash Kumar <akashkumar59200@gmail.com>
2022-03-03 01:16:58 +0530

    twilio integration, mongoose, backend setup

---
+++ backend/controllers/auth-controller.js
@@ -0,0 +1,68 @@
+const otpService = require("../services/otp-service");
+const hashService = require("../services/hash-service");
+const userService = require("../services/user-service");
+
+class AuthController {
+  async sendOtp(req, res) {
+    // Logic
+    const { phone } = req.body;
+    if (!phone) {
+      res.status(400).json({ message: "Phone field is required!" });
+    }
+
+    const otp = await otpService.generateOtp();
+
+    // Hash
+    const ttl = 1000 * 60 * 2; // 2min
+    const expires = Date.now() + ttl;
+    const data = `${phone}.${otp}.${expires}`;
+    const hash = hashService.hashOtp(data);
+
+    // send OTP
+    try {
+      await otpService.sendBySms(phone, otp);
+      res.json({
+        hash: `${hash}.${expires}`,
+        phone,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "message sending failed" });
+    }
+  }
+
+  async verifyOtp(req, res) {
+    // Logic
+    const { otp, hash, phone } = req.body;
+    if (!otp || !hash || !phone) {
+      res.status(400).json({ message: "All fields are required!" });
+    }
+
+    const [hashedOtp, expires] = hash.split(".");
+    if (Date.now() > +expires) {
+      res.status(400).json({ message: "OTP expired" });
+    }
+
+    const data = `${phone}.${otp}.${expires}`;
+    const isValid = otpService.verifyOtp(hashedOtp, data);
+    if (!isValid) {
+      res.status(400).json({ message: "Invalid OTP" });
+    }
+
+    let user;
+    let accessToken;
+    let refreshToken;
+    try {
+            user = await userService.findUser({ phone });
+            if (!user) {
+                user = await userService.createUser({ phone });
+            }
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ message: 'Db error' });
+        }
+
+    // Token
+  }
+}
+module.exports = new AuthController();
